2e0017a24136989dbdd3e24e543d82e02fcdf63b
Milica Drača <draca.milica10@gmail.com>
2023-01-14 03:24:10 +0100

    Backend Progress

--- src/services/rides/src/main/java/com/uber/rides/controller/Drivers.java
+++ src/services/rides/src/main/java/com/uber/rides/controller/Drivers.java
@@ -1,10 +1,9 @@
 package com.uber.rides.controller;
 
-import static com.uber.rides.util.Utils.*;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,13 +11,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.uber.rides.database.DbContext;
-import com.uber.rides.dto.user.DriverDTO;
+import com.uber.rides.dto.user.UserDTO;
 import com.uber.rides.model.User;
 import com.uber.rides.model.User$;
 import com.uber.rides.model.User.Roles;
 import com.uber.rides.ws.Store;
 import com.uber.rides.ws.WS;
 import com.uber.rides.ws.driver.DriverData;
+
+import static com.uber.rides.util.Utils.*;
 
 @RestController
 @RequestMapping("/drivers")
@@ -39,29 +40,35 @@ public class Drivers extends Controller {
     }
 
     @Transactional
-    @GetMapping("/")
+    @GetMapping()
     @Secured({ Roles.ADMIN })
     public Object getDrivers(@RequestParam int page, @RequestParam String criteria, @RequestParam String query) {
-        
+        var hasText = StringUtils.hasText(query);
         if (criteria.equalsIgnoreCase("ACTIVE")) {
             return store.drivers
                 .values()
                 .stream()
                 .map(DriverData::getUser)
                 .filter(u -> 
-                    (u.getFirstName() + u.getLastName())
+                    hasText &&
+                    (u.getFirstName() + u.getLastName() + u.getEmail())
                     .toUpperCase()
                     .contains(query.toUpperCase())
                 )
                 .sorted((d1, d2) -> d1.getFirstName().compareTo(d2.getFirstName()))
                 .skip(page * PAGE_SIZE)
                 .limit(PAGE_SIZE)
-                .map(driver -> mapper.map(driver, DriverDTO.class))
+                .map(driver -> {
+                    var dto = mapper.map(driver, UserDTO.class);
+                    dto.setOnline(store.drivers.containsKey(driver.getId()));
+                    return dto;
+                })
                 .toList();
         }
 
         return context.query()
             .stream(User.class)
+            .filter(User$.role.equal(Roles.DRIVER))
             .filter(
                 User$.firstName.containsIgnoreCase(query).or(
                 User$.lastName.containsIgnoreCase(query))
@@ -70,8 +77,10 @@ public class Drivers extends Controller {
             .skip(page * PAGE_SIZE)
             .limit(PAGE_SIZE)
             .map(driver -> {
-                driver.setCurrentTrip(ws.getCurrentTrip(driver));
-                return mapper.map(driver, DriverDTO.class);
+                // driver.setCurrentTrip(ws.getCurrentTrip(driver));
+                var dto = mapper.map(driver, UserDTO.class);
+                dto.setOnline(store.drivers.containsKey(driver.getId()));
+                return dto;
             })
             .toList();
 
